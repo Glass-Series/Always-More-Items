7c04e668e2cf9af3bdc7f1cb660d9a1b7d4bdd1e
calmilamsy <bumbill00@gmail.com>
2024-08-07 19:58:54 +0100

    Fix a rendering bug

--- src/main/java/net/glasslauncher/mods/alwaysmoreitems/mixin/client/class_555Mixin.java
+++ src/main/java/net/glasslauncher/mods/alwaysmoreitems/mixin/client/class_555Mixin.java
@@ -3,6 +3,7 @@ package net.glasslauncher.mods.alwaysmoreitems.mixin.client;
 import net.glasslauncher.mods.alwaysmoreitems.gui.screen.OverlayScreen;
 import net.minecraft.class_555;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -12,7 +13,7 @@ public class class_555Mixin {
 
     @Redirect(method = "method_1844", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;render(IIF)V"))
     private void iAmNoLongerSuggesting(Screen instance, int mouseX, int mouseY, float delta) {
-        if (OverlayScreen.INSTANCE.recipesGui.isActive()) {
+        if (OverlayScreen.INSTANCE.recipesGui.isActive() && instance instanceof HandledScreen) {
             OverlayScreen.INSTANCE.render(mouseX, mouseY, delta);
             return;
         }
