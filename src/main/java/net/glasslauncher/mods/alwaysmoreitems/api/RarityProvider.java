ce15518a9357184e0d89a1ed99ae8071b805ff9b
calmilamsy <bumbill00@gmail.com>
2026-04-19 14:18:40 +0100

    Javadoc casually spreading misinformation, link to new method

--- src/main/java/net/glasslauncher/mods/alwaysmoreitems/api/RarityProvider.java
+++ src/main/java/net/glasslauncher/mods/alwaysmoreitems/api/RarityProvider.java
@@ -3,10 +3,10 @@ package net.glasslauncher.mods.alwaysmoreitems.api;
 import net.minecraft.item.ItemStack;
 
 /**
- * You *can* just put an enum value into your item names, as that's what's done internally, but this is cleaner for your code.
+ * You *can* just put an enum value into your item names, but this is cleaner for your code.
+ * Use {@link ItemRarityProvider} instead.
  */
 @Deprecated(forRemoval = true)
 public interface RarityProvider {
-
     Rarity getRarity(ItemStack itemStack);
 }
