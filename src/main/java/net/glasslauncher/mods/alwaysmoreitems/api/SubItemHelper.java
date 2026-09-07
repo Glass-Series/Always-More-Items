45da0371e4bd72f2567335c76f51e9b65a64ac3c
Daniel Fildán <fildand@centrum.cz>
2026-09-07 22:55:05 +0200

    Give helpful error messages when loading SubItemProvider fails

--- src/main/java/net/glasslauncher/mods/alwaysmoreitems/api/SubItemHelper.java
+++ src/main/java/net/glasslauncher/mods/alwaysmoreitems/api/SubItemHelper.java
@@ -1,26 +1,28 @@
 package net.glasslauncher.mods.alwaysmoreitems.api;
 
-import lombok.SneakyThrows;
 import net.glasslauncher.mods.alwaysmoreitems.util.AlwaysMoreItems;
 import net.glasslauncher.mods.alwaysmoreitems.util.MethodFinder;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.modificationstation.stationapi.api.registry.ItemRegistry;
 
 import javax.annotation.Nullable;
 import java.lang.reflect.Method;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 public class SubItemHelper {
-    @SneakyThrows // Bad access? Perish.
     public static @Nullable List<ItemStack> getSubItems(Item item) {
-        Method method = MethodFinder.findMethodWithAnnotation(item.getClass(), SubItemProvider.class);
-        if (method != null) {
-            AlwaysMoreItems.LOGGER.info("Found SubItemProvider method for {}", item.getClass());
-            //noinspection unchecked Wrong type? Believe it or not, also perish.
-            return (List<ItemStack>) method.invoke(item);
+        try {
+            Method method = MethodFinder.findMethodWithAnnotation(item.getClass(), SubItemProvider.class);
+            if (method != null) {
+                AlwaysMoreItems.LOGGER.info("Found SubItemProvider method for {} ({})", item.getClass(), ItemRegistry.INSTANCE.getId(item));
+                //noinspection unchecked Wrong type? Believe it or not, also perish.
+                return (List<ItemStack>) method.invoke(item);
+            }
+        } catch (Exception | LinkageError e) {
+            AlwaysMoreItems.LOGGER.error("Failed to get SubItemProvider for {} ({})", item.getClass(), ItemRegistry.INSTANCE.getId(item), e);
         }
+        
         return null;
     }
 }
